4ccccf81247588a07e69862b732f9d29e8d9f98f
Jaime Andres Castillo Leon <jacastillol@gmail.com>
2017-09-15 16:11:57 -0500

    Add struct RobotModel to define robot

--- C++/screwSchoolUN002.cpp
+++ C++/screwSchoolUN002.cpp
@@ -10,8 +10,36 @@
 using namespace RigidBodyDynamics;
 using namespace RigidBodyDynamics::Math;
 
+struct RobotModel {
+  RobotModel(int dofs, double* q, double* L, Vector3d* r)
+    : dof(dofs) {
+    for ( int i=0; i<dofs; i++ ) {
+      qi[i] = q[i]; li[i] = L[i];
+      XJi[i] = Xrotz(qi[i]); XTi[i] = Xtrans(r[i]);
+      Xi[i] = XJi[i]*XTi[i];
+    }
+  };
+  int dof;
+  double qi[2];
+  double li[2];
+  SpatialTransform XJi[2];
+  SpatialTransform XTi[2];
+  SpatialTransform Xi[2];
+};
+
 int main (int argc, char* arg[]) {
   rbdl_check_api_version(RBDL_API_VERSION);
+
+  int dofs = 2;
+  double q[dofs] = {M_PI/6,M_PI/4};
+  double L[dofs] = {0.5,0.3};
+  Vector3d r[dofs] = {Vector3d(L[0],0.,0.), Vector3d(L[1],0.,0.)};
+  
+  RobotModel robot( dofs, q, L, r );
+
+  std::cout << "==========================================================" << std::endl;
+  std::cout << "        Using Spatial Math Utils "
+	    << std::endl << std::endl;
 
 
   return 0;
