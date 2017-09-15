c03879bb8bdf5dbf1b8cfa5d0ac932b16482bff5
Jaime Andres Castillo Leon <jacastillol@gmail.com>
2017-09-15 16:09:45 -0500

    Add second example

---
+++ C++/screwSchoolUN002.cpp
@@ -0,0 +1,18 @@
+/*
+  How to do: 
+  6D basic oprations in a planar robot with 2dofs. 
+  Inverse and direct kinematics in position and velocity in different frames.
+ */
+#include <iostream>
+
+#include <rbdl/rbdl.h>
+
+using namespace RigidBodyDynamics;
+using namespace RigidBodyDynamics::Math;
+
+int main (int argc, char* arg[]) {
+  rbdl_check_api_version(RBDL_API_VERSION);
+
+
+  return 0;
+}
