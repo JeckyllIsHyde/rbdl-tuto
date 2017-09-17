0a7dba628c46bc49fb6d1c234f1cb19bdab5e276
Jaime Andres Castillo Leon <jacastillol@gmail.com>
2017-09-17 15:59:52 -0500

    feat: Add example for statics an force transform ops

---
+++ C++/screwSchoolUN003.cpp
@@ -0,0 +1,20 @@
+/*
+  How to do: 
+  6D basic oprations in a Robot2R for its statics.
+ */
+#include <iostream>
+#include <cmath>
+#include <vector>
+
+#include <rbdl/rbdl.h>
+
+using namespace RigidBodyDynamics;
+using namespace RigidBodyDynamics::Math;
+using namespace RigidBodyDynamics::Utils;
+
+int main (int argc, char* arg[]) {
+  rbdl_check_api_version(RBDL_API_VERSION);
+
+  
+  return 0;
+}
