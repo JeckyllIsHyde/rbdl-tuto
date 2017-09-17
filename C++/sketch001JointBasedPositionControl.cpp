f68995b6637c526f5d04ff4c60ca79f2ff5b284d
Jaime Andres Castillo Leon <jacastillol@gmail.com>
2017-09-17 16:53:03 -0500

    feat: Add a position control example

---
+++ C++/sketch001JointBasedPositionControl.cpp
@@ -0,0 +1,27 @@
+/*
+  How to do: 
+  A position control for a robot2R planar
+
+  
+ */
+
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <ctime>
+#include <boost/numeric/odeint.hpp>
+#include <rbdl/rbdl.h>
+
+using namespace RigidBodyDynamics;
+using namespace RigidBodyDynamics::Math;
+using namespace RigidBodyDynamics::Utils;
+
+using namespace boost::numeric::odeint;
+
+int main (int argc, char* arg[]) {
+  rbdl_check_api_version(RBDL_API_VERSION);
+
+  
+  
+  return 0;
+}
