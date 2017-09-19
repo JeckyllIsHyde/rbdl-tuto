d33dadddbe5285075440799446c75bf4ad4cfff4
Jaime Andres Castillo Leon <jacastillol@gmail.com>
2017-09-19 12:18:39 -0500

    feat: Add transpose jacobian with PD+CG example

--- C++/sketch001JointBasedPositionControl.cpp
+++ C++/sketch001JointBasedPositionControl.cpp
@@ -146,6 +146,31 @@ void TransposeJacobianPControllerFcn ( Model& model,
   tau = J.transpose()*f;
 }
 
+void TransposeJacobianPDwGCControllerFcn ( Model& model,
+					   const VectorNd& q, const VectorNd& qd,
+					   const VectorNd& xD, const VectorNd& ,const VectorNd& ,
+					   VectorNd& tau,
+					   std::vector<Math::SpatialVector>& f_ext ) {
+
+  MatrixNd J = MatrixNd::Zero ( 6, model.dof_count ),
+    Jp = J, invJp = J;
+
+  Vector3d ph( 0.3,0.0,0.0 );
+  VectorNd zero = VectorNd::Zero (model.dof_count);
+  double kp = 10.0, kd = 0.8;
+
+  unsigned int body_2_id = model.GetBodyId("body_2");
+  J.setZero();
+  CalcBodySpatialJacobian( model, q, body_2_id, J, true );
+  J = model.X_base[2].inverse().toMatrix()*J;
+  Vector3d p = CalcBodyToBaseCoordinates( model, q, body_2_id, ph, false ),
+    fu = kp*( xD-p )
+    + kd*(/*zero*/-(Xtrans(p).toMatrix()*J).block(3,0,3,2)*qd);
+  SpatialVector f;
+  f << VectorCrossMatrix(p)*fu, fu;
+  tau = J.transpose()*f;
+}
+
 void InverseJacobianPDControllerFcn ( Model& model,
 				      const VectorNd& q, const VectorNd& qd,
 				      const VectorNd& xD, const VectorNd& ,const VectorNd& ,
@@ -291,9 +316,11 @@ int main (int argc, char* arg[]) {
   // Choose: qDesiredForRegulationFcn,  qDesiredForTrackingFcn, xDesiredForRegulationFcn,
   //         xDesiredForTrackingFcn
   // Choose: PDControllerFcn, PDWithGCControllerFcn, PDWithIDControllerFcn,
-  //         TransposeJacobianPControllerFcn, InverseJacobianPDControllerFcn
-  // DynRobotFunctor dynRobotFnc( &robot2R, PDWithIDControllerFcn, qDesiredForTrackingFcn );
-  DynRobotFunctor dynRobotFnc( &robot2R, TransposeJacobianPControllerFcn, xDesiredForRegulationFcn );
+  //         TransposeJacobianPControllerFcn, InverseJacobianPDControllerFcn,
+  //         TransposeJacobianPDwGCControllerFcn
+  DynRobotFunctor dynRobotFnc( &robot2R,
+			       TransposeJacobianPDwGCControllerFcn,
+			       xDesiredForRegulationFcn );
   double t = 0.0, t_init = 0.0, t_end = 10.0, dt = 0.01;
   Estado_type x(2*robot2R.dof_count);
 
