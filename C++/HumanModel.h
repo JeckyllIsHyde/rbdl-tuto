da2269ae1fa749b532998e3dc8c7492296964edc
Jaime Andres Castillo Leon <jacastillol@gmail.com>
2017-11-09 12:40:10 -0500

    feat: Add Human model from RBDL addon

---
+++ C++/HumanModel.h
@@ -0,0 +1,10 @@
+#ifndef _HUMANMODEL_H
+#define _HUMANMODEL_H
+
+namespace RigidBodyDynamics {
+class Model;
+}
+
+void generate_human36model (RigidBodyDynamics::Model *model);
+
+#endif /* _HUMANMODEL_H */
