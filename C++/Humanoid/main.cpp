abbfbea2c7740f584ec994f9b7efd222cdef5551
Jaime Andres <jacastillol@gmail.com>
2017-11-21 14:06:30 -0500

    feat: Init generalized variables of a RBDL model with quaternions

--- C++/Humanoid/main.cpp
+++ C++/Humanoid/main.cpp
@@ -10,7 +10,20 @@ using namespace RigidBodyDynamics::Addons;
 const double dt = 0.001;
 const double tmax = 5.0;
 
+struct MechTreeSystem {
+
+  typedef std::vector<SpatialVector> ForceContainer;
+
+  Model model;
+  VectorNd q, qd, qdd, tau;
+  ForceContainer f_ext;
+
+  void initGeneralizedVariables();
+};
+
 struct PhysicsEngine {
+  MechTreeSystem mechSys;
+  
   void printData( double t ) {}
   void update( double dt ) {}
 };
@@ -27,6 +40,12 @@ void init_engine_with_humanoid( PhysicsEngine& engine ) {
     if (human->mBodies[id].mMass==0.0)
       human->mBodies[id].mIsVirtual = true;
 
+  // initialize q,qd,qdd,tau and Q
+  engine.mechSys.initGeneralizedVariables();
+
+  std::cout << "q: " << engine.mechSys.q.transpose() << std::endl;
+  std::cout << "qd: " << engine.mechSys.qd.transpose() << std::endl;
+  std::cout << "qdd: " << engine.mechSys.qdd.transpose() << std::endl;
 }
 
 int main() {
@@ -46,4 +65,25 @@ int main() {
   engine.printData( t );
   
   return 0;
+}
+
+void MechTreeSystem::initGeneralizedVariables() {
+  q = VectorNd::Zero (model.q_size);
+  qd = VectorNd::Zero (model.qdot_size);
+  qdd = VectorNd::Zero (model.qdot_size);
+  tau = VectorNd::Zero (model.qdot_size);
+
+  /*
+  model.SetQuaternion( model.GetBodyId("pelvis"),
+		       Quaternion::fromZYXAngles( Vector3dZero ),
+		       q );
+  */
+
+  for ( int i; i<model.mBodies.size(); i++ )
+    if ( model.mJoints[i].mJointType==JointTypeSpherical )
+      model.SetQuaternion( i,
+			   Quaternion::fromZYXAngles( Vector3dZero ),
+			   q );
+
+  f_ext = ForceContainer( model.mBodies.size(), SpatialVectorZero );
 }
