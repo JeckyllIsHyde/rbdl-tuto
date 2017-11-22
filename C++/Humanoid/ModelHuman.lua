771399022d6730b153d15116874a34a93fe23eb1
Jaime Andres <jacastillol@gmail.com>
2017-11-22 09:00:17 -0500

    feat: Add right shank and right foot to model

--- C++/Humanoid/ModelHuman.lua
+++ C++/Humanoid/ModelHuman.lua
@@ -1,6 +1,7 @@
 joints = {
   j_spherical = { "JointTypeSpherical" },
   --j_spherical = { "JointTypeEulerZYX" },
+  j_rot_y = { {0.0,1.0,0.0,0.0,0.0,0.0}, },
 }
 
 model = {
@@ -98,7 +99,89 @@ model = {
       },
     },
 
-  },
+    {
+      name = "shank_r",
+      parent = "thigh_r",
+      body = {
+        mass = 1.0,
+	com = {0.0,0.0,-0.18},
+	iniertia = {
+	  {0.1,0.0,0.0},
+	  {0.0,0.1,0.0},
+	  {0.0,0.0,0.1},
+	},
+      },
+      joint = joints.j_rot_y,
+      joint_frame = {
+        r = {0.0,0.0,-0.4},
+      },
+      visuals = {
+        {
+	  color = { 0.8,0.8,0.2 },
+	  translate = { 0.0,0.0,-0.18 },
+	  geometry = {
+	    sphere = { radius=0.05 },
+	  },
+	},
+        {
+	  color = { 0.2,0.8,0.2 },
+	  translate = { 0.0,0.0,-0.36 },
+	  geometry = {
+	    sphere = { radius=0.05 },
+	  },
+	},
+      },
+    },
+
+    {
+      name = "foot_r",
+      parent = "shank_r",
+      body = {
+        mass = 1.0,
+	com = {0.1,0.0,-0.08},
+	iniertia = {
+	  {0.1,0.0,0.0},
+	  {0.0,0.1,0.0},
+	  {0.0,0.0,0.1},
+	},
+      },
+      joint = joints.j_rot_y,
+      joint_frame = {
+        r = {0.0,0.0,-0.4},
+      },
+      visuals = {
+        {
+	  color = { 0.8,0.8,0.2 },
+	  translate = { 0.1,0.0,-0.08 },
+	  geometry = {
+	    sphere = { radius=0.01 },
+	  },
+	},
+        {
+	  color = { 0.2,0.8,0.2 },
+	  translate = { -0.01,0.0,-0.1 },
+	  geometry = {
+	    sphere = { radius=0.01 },
+	  },
+	},
+        {
+	  color = { 0.2,0.8,0.2 },
+	  translate = { 0.2,-0.03,-0.1 },
+	  geometry = {
+	    sphere = { radius=0.01 },
+	  },
+	},
+        {
+	  color = { 0.2,0.8,0.2 },
+	  translate = { 0.2,0.03,-0.1 },
+	  geometry = {
+	    sphere = { radius=0.01 },
+	  },
+	},
+      },
+    },
+
+},
 }
 
 return model
