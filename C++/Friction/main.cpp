e01d630ee47e04e196252be810685f5d6364e643
Jaime Andres <jacastillol@gmail.com>
2017-12-07 19:19:46 -0500

    test: Add step for single rigid body

--- C++/Friction/main.cpp
+++ C++/Friction/main.cpp
@@ -1,9 +1,18 @@
 /*
   Collision and friction for rigid multi-body dynamics
 
-   - Screw-based formulation
+  - Screw-based formulation
+
+  - Global frame coords (simplify for 2D with X-Z)
+
+  Z^
+  |
+  Yx---> X
 */
 #include <iostream>
+#include <rbdl/rbdl.h>
+
+using namespace RigidBodyDynamics::Math;
 
 const double dt = 0.01;
 const double tmax = 5.0;
@@ -12,10 +21,15 @@ struct OneBody {
   double mMass;
   Matrix3d mInertia;
   SpatialVector v, a, f;
-  SpatialTransform X;  
+  SpatialTransform X;
+
+  void forwardDynamics( void );
+  void step( double dt );
 };
 
 struct PhysicsEngine {
+  OneBody body;
+
   void update( double t );
   void printData( double dt );
 };
@@ -39,7 +53,28 @@ int main() {
   return 0;
 };
 
-void init_engine_with_rod( PhysicsEngine& engine ) {}
+void init_engine_with_rod( PhysicsEngine& engine ) {
+  engine.body.mMass = 1.0;
+  engine.body.mInertia = Matrix3d(0.0,0.0,0.0,
+				  0.0,0.002,0.0,
+				  0.0,0.0,0.0);
+  engine.body.v=engine.body.a=engine.body.f  = SpatialVectorZero;
+  engine.body.X.E = Matrix3dIdentity;
+  engine.body.X.r = Vector3dZero;
+}
 
-void PhysicsEngine::update( double t ) {}
-void PhysicsEngine::printData( double dt ) {}
+void PhysicsEngine::update( double dt ) {
+  body.forwardDynamics();
+  body.step( dt );
+}
+
+void PhysicsEngine::printData( double t ) {}
+
+void OneBody::forwardDynamics( void ) {
+  //  a = FD(q,v,f)
+}
+
+void OneBody::step( double dt ) {
+  //  q += dt*v;
+  v += dt*a;
+}
