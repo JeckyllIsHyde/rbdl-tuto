7dc7471147d9232f04630258bbb7a332bdd562a5
Jaime Andres <jacastillol@gmail.com>
2017-12-08 19:52:39 -0500

    feat: Add lua-file for rod animation

---
+++ C++/Friction/Rod.lua
@@ -0,0 +1,40 @@
+model = {
+  configuration = {
+    axis_front = { 1, 0, 0 },
+    axis_up    = { 0, 0, 1 },
+    axis_right = { 0, -1, 0 },
+    rotation_order = { 1, 2, 0},
+  },
+  gravity = {0.0,0.0,-10.0},
+  frames = {
+  
+    {
+      name = "rod",
+      parent = "ROOT",
+      body = {
+        mass = 0.0
+      },
+      visuals = {
+        {
+	  color = { 0.8, 0.2, 0.8 },
+	  rotate = {
+	    axis = {0.0,1.0,0.0},
+	    angle = 90
+	  },
+	  geometry = {
+	    capsule = { radius=0.05, length=0.51 }
+	  },
+	},
+        {
+	  color = { 0.2, 0.8, 0.8 },
+	  geometry = {
+	    sphere = { radius=0.1 }
+	  },
+	},
+      },
+    },
+    
+  },
+}
+
+return model
